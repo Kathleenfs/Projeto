6a5590ffb4dbe728843ed1ccadc3ce100756df4f
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-08-26 14:25:24 -0300

    Entidades Criadas
    Classe cliente preenchida

---
+++ projeto/src/main/java/com/petshop/projeto/entities/Animal.java
@@ -0,0 +1,5 @@
+package com.petshop.projeto.entities;
+
+public class Animal {
+
+}
---
+++ projeto/src/main/java/com/petshop/projeto/entities/Cliente.java
@@ -0,0 +1,41 @@
+package com.petshop.projeto.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente {
+
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String nome;
+	private  int telefone;
+	
+	
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+}
